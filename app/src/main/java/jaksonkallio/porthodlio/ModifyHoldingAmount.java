06cf1f3a39071577edb11ae2afee6efd5570cfb7
Jakson Kallio <jakson@protonmail.com>
2017-12-20 13:29:50 -0600

    ModifyHoldingAmount base

---
+++ app/src/main/java/jaksonkallio/porthodlio/ModifyHoldingAmount.java
@@ -0,0 +1,13 @@
+package jaksonkallio.porthodlio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyHoldingAmount extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_modify_holding_amount);
+	}
+}
