7165bc1ee553140e4308f7738012e94b18a3182d
Jakson Kallio <jakson@protonmail.com>
2017-12-19 19:14:41 -0600

    CoinDatabase updating

--- app/src/main/java/jaksonkallio/porthodlio/CoinDatabase.java
+++ app/src/main/java/jaksonkallio/porthodlio/CoinDatabase.java
@@ -1,5 +1,7 @@
 package jaksonkallio.porthodlio;
 
+import android.os.AsyncTask;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,47 +20,8 @@ import java.util.ArrayList;
 
 public class CoinDatabase {
 	public static void updatePrices(){
-		if(Timing.getEpochMinute() >= timeNextUpdate()){
-			try {
-				URL ticker_endpoint = new URL("https://api.coinmarketcap.com/v1/ticker/");
-				URLConnection conn = ticker_endpoint.openConnection();
-				BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-				setLastUpdateTime();
-				String input_line;
-				String raw_json = "";
-
-				while ((input_line = br.readLine()) != null) {
-					raw_json += input_line;
-				}
-
-				JSONArray coin_list = new JSONArray(raw_json);
-
-				for(int i = 0; i < coin_list.length(); i++) {
-					if (coin_list.get(i) instanceof JSONObject) {
-						JSONObject coin_entry = coin_list.getJSONObject(i);
-
-						if (!coin_entry.isNull("id") && !coin_entry.isNull("name") && !coin_entry.isNull("price_usd") && !coin_entry.isNull("percent_change_24h")) {
-							String ticker = coin_entry.getString("symbol").toLowerCase();
-							String name = coin_entry.getString("name");
-							double price = Double.parseDouble(coin_entry.getString("price_usd"));
-							long market_cap = (long) Double.parseDouble(coin_entry.getString("market_cap_usd"));
-							long volume = (long) Double.parseDouble(coin_entry.getString("24h_volume_usd"));
-							double percent_change_24h = Double.parseDouble(coin_entry.getString("percent_change_24h"));
-							int rank = Integer.parseInt(coin_entry.getString("rank"));
-
-							updateCoin(ticker, name, price, market_cap, volume, percent_change_24h, rank);
-						}
-					}
-				}
-			} catch (MalformedURLException ex) {
-				System.out.println("Bad URL: " + ex.getMessage());
-			} catch (IOException ex) {
-				System.out.println("Bad stream: " + ex.getMessage());
-			} catch (JSONException ex) {
-				System.out.println("Bad JSON: " + ex.getMessage());
-			}
-
-			System.out.println(coins);
+		if(Timing.getEpochMinute() >= timeNextUpdate()) {
+			new RetrieveMarketDataTask().execute("");
 		}
 	}
 
@@ -105,6 +68,7 @@ public class CoinDatabase {
 	}
 
 	public static Coin getCoin(int list_position){
+		System.out.println("Fetching coin from database, position: "+list_position);
 		return coins.get(list_position);
 	}
 
@@ -115,4 +79,61 @@ public class CoinDatabase {
 	private static int last_update_time = 0;
 	public final static int UPDATE_INTERVAL = 15;
 	private static ArrayList<Coin> coins = new ArrayList<>();
+
+	private static class RetrieveMarketDataTask extends AsyncTask<String, Void, JSONArray> {
+
+		@Override
+		protected JSONArray doInBackground(String... strings) {
+			try {
+				URL ticker_endpoint = new URL("https://api.coinmarketcap.com/v1/ticker/");
+				URLConnection conn = ticker_endpoint.openConnection();
+				BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				setLastUpdateTime();
+				String input_line;
+				String raw_json = "";
+
+				while ((input_line = br.readLine()) != null) {
+					raw_json += input_line;
+				}
+
+				JSONArray coin_list = new JSONArray(raw_json);
+
+				return coin_list;
+
+			} catch (MalformedURLException ex) {
+				System.out.println("Bad URL: " + ex.getMessage());
+			} catch (IOException ex) {
+				System.out.println("Bad stream: " + ex.getMessage());
+			} catch (JSONException ex) {
+				System.out.println("Bad JSON: " + ex.getMessage());
+			}
+
+			return new JSONArray();
+		}
+
+		@Override
+		protected void onPostExecute(JSONArray coin_list){
+			for(int i = 0; i < coin_list.length(); i++) {
+				try {
+					if (coin_list.get(i) instanceof JSONObject) {
+						JSONObject coin_entry = coin_list.getJSONObject(i);
+
+						if (!coin_entry.isNull("id") && !coin_entry.isNull("name") && !coin_entry.isNull("price_usd") && !coin_entry.isNull("percent_change_24h")) {
+							String ticker = coin_entry.getString("symbol").toLowerCase();
+							String name = coin_entry.getString("name");
+							double price = Double.parseDouble(coin_entry.getString("price_usd"));
+							long market_cap = (long) Double.parseDouble(coin_entry.getString("market_cap_usd"));
+							long volume = (long) Double.parseDouble(coin_entry.getString("24h_volume_usd"));
+							double percent_change_24h = Double.parseDouble(coin_entry.getString("percent_change_24h"));
+							int rank = Integer.parseInt(coin_entry.getString("rank"));
+
+							updateCoin(ticker, name, price, market_cap, volume, percent_change_24h, rank);
+						}
+					}
+				} catch (JSONException e) {
+					System.out.println("Bad JSON: " + e.getMessage());
+				}
+			}
+		}
+	}
 }
