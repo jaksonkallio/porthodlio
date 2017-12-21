b63762bcf047b92c4b557db3681ea88d1dce9195
Jakson Kallio <jakson@protonmail.com>
2017-12-21 13:08:58 -0600

    Fill in field details of coin

--- app/src/main/java/jaksonkallio/porthodlio/ModifyHoldingAmount.java
+++ app/src/main/java/jaksonkallio/porthodlio/ModifyHoldingAmount.java
@@ -2,6 +2,8 @@ package jaksonkallio.porthodlio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ModifyHoldingAmount extends AppCompatActivity {
 
@@ -11,6 +13,14 @@ public class ModifyHoldingAmount extends AppCompatActivity {
 		setContentView(R.layout.activity_modify_holding_amount);
 		setTitle("Modify Holding Amount");
 		coin = CoinDatabase.getCoin(getIntent().getExtras().getString("ticker"));
+		TextView name = (TextView) findViewById(R.id.name);
+		TextView ticker = (TextView) findViewById(R.id.ticker);
+		TextView price = (TextView) findViewById(R.id.price);
+		EditText amount = (EditText) findViewById(R.id.amount);
+
+		name.setText(coin.getName());
+		ticker.setText(coin.getTicker());
+		price.setText(DisplayPreferences.formatPrice(coin.getPrice()));
 	}
 
 	private Coin coin;
