2540eb63d7bab234864d992abb2154781691a79a
Jakson Kallio <jakson@protonmail.com>
2017-12-21 13:06:05 -0600

    Coin modify amount activity base

--- app/src/main/java/jaksonkallio/porthodlio/ModifyHoldingAmount.java
+++ app/src/main/java/jaksonkallio/porthodlio/ModifyHoldingAmount.java
@@ -9,5 +9,9 @@ public class ModifyHoldingAmount extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_modify_holding_amount);
+		setTitle("Modify Holding Amount");
+		coin = CoinDatabase.getCoin(getIntent().getExtras().getString("ticker"));
 	}
+
+	private Coin coin;
 }
